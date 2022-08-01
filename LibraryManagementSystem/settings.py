de7883bd4c4b6bbd8e8ba14eb0d652db5f758406
BOGERAHENRY <bogerahenry333@gmail.com>
2022-08-01 16:45:10 +0300

    true

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(*h%h-rc^kh_ob18is3j+c6@*3b92&9p^%s2lf^qas^lk3*n_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
